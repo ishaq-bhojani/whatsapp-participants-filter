a26d1611585adf60d82b3ac6cd8c2a02df3d5001
Ishaq Bhojani <ibhojani@nisum.com>
2023-04-09 20:46:49 +0500

    init

---
+++ app.js
@@ -0,0 +1,37 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const myGroupName = ""; // Group name in String
+
+const participantsToKeep = require('./participants.json'); // Array of participants to keep
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+  client.getChats().then((chats) => {
+    const myGroup = chats.find(
+      (chat) => chat.name.toLowerCase() === myGroupName.toLowerCase()
+    );
+    const participants = myGroup.participants;
+    const participantsToRemove = [];
+    participants.forEach(p => {
+      if (participantsToKeep.indexOf(p.id.user) === -1) {
+        if(!p.isAdmin && !p.isSuperAdmin) {
+          participantsToRemove.push(p.id._serialized);
+        }
+      }
+    });
+    myGroup.removeParticipants(participantsToRemove)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+  });
+});
+
+client.initialize();
